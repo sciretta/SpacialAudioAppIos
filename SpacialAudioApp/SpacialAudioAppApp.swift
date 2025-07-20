cd33266c3ede85c825430e67f4694387dbc0332e
leo <sciretta28@gmail.com>
2025-07-20 16:58:27 -0400

    Initial Commit

---
+++ SpacialAudioApp/SpacialAudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacialAudioAppApp.swift
+//  SpacialAudioApp
+//
+//  Created by Leonardo on 20/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SpacialAudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpacialAudioAppTests/SpacialAudioAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpacialAudioAppTests.swift
+//  SpacialAudioAppTests
+//
+//  Created by Leonardo on 20/7/25.
+//
+
+import Testing
+@testable import SpacialAudioApp
+
+struct SpacialAudioAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
